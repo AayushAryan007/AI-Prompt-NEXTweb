2498bdb5384331985231fd38ef828c4ea9742e73
Ayush <unitedestates.ayush10@gmail.com>
2023-08-07 01:16:25 +0530

    app initiated 5

---
+++ my-app/app/post/new/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+// /posts/New
+const New = () => {
+  return <div>new</div>;
+};
+
+export default New;
---
+++ my-app/app/post/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+// /posts
+const post = () => {
+  return <div>post</div>;
+};
+
+export default post;
